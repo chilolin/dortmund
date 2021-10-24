cdfafeb9d5fa26aab801ecebf84e83c722a952ba
unknown <yanagi.taka.291@s.kyushu-u.ac.jp>
2021-10-24 16:48:39 +0900

    [add]HPで音を出す
    publicに直接jsファイルを記述

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <script src="/js/function-play-sound.js"></script>
+</head>
+<body>
+  <h1>Dortmund</h1>
+  <button id="sound" onclick="playChord(394.46)">E4</button>
+  <button id="sound" onclick="downPlayChord(392.0,246.94,196.0,146.83,123.47,98.0);">up-G-Chord</button>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\homeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('justsound', homeController::class);
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
