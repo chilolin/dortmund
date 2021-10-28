cf04a864c36325c2f7bc0e3ede5bd083a75b3268
unknown <yanagi.taka.291@s.kyushu-u.ac.jp>
2021-10-28 15:29:38 +0900

    [add]メロから音を生成
    unit_timeでテンポも調整可

--- resources/views/mero_created.blade.php
+++ resources/views/mero_created.blade.php
@@ -1,13 +1,39 @@
+<?php
+
+$json_array = json_encode($merofreqs);
+// var_dump($json_array);
+// exit();
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="{{ asset('/js/function-play-sound.js') }}"></script>
 
         <title>Select Chords</title>
     </head>
+
     <body>
-        {{-- @dd($_GET) --}}
-        @dd($merofreqs);
+        <script>
+            let js_array = <?php echo $json_array; ?>;
+            console.log(js_array); 
+            PlayChord(js_array,0.1);
+        </script>
+        <button onclick="PlayChord(js_array,0.1);">Replay</button>
+        <!-- {{-- @dd($_GET) --}} -->
+        <!-- @dd($merofreqs); -->
+    
+        
+        
+        
+        
+        
+
+
+        
     </body>
 </html>
